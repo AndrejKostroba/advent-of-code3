125f8872f403602417b422e2bd7b02ae46274b52
0xblazy <carbonnier.nico@gmail.com>
2023-12-03 11:45:17 +0100

    ✨ day 3 - gear ratios

--- 2023/day-0/model.py
+++ 2023/day-0/model.py
@@ -6,7 +6,7 @@ file = pathlib.Path(__file__).parent.resolve().joinpath('input.txt')
 
 with open(file) as f:
     lines = f.readlines()
-    part1, part2 = '', ''
+    part1, part2 = 0, 0
 
     print('Part 1 :', part1)
     print('Part 2 :', part2)
